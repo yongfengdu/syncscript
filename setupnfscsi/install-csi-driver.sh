b5de3b03923d6afdc2321585ca1c02e73c0d5a63
Dolpher Du <dolpher.du@intel.com>
2025-07-02 03:20:35 +0000

    nfs csi setup script
    Signed-off-by: Dolpher Du <dolpher.du@intel.com>

---
+++ setupnfscsi/install-csi-driver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
+helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs --namespace kube-system --version 4.11.0
